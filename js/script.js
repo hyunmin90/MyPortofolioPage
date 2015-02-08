ba817007422b11d0a353fd1544f8fe42e4bfdffc
hyunmin90 <hyunmin90@gmail.com>
2015-02-08 06:45:16 +0000

    edited

--- js/script.js
+++ js/script.js
@@ -1,16 +1,63 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
 
-function highlightThis(event) {
-    //event.stopPropagation();
+
+var Portofolio = {};
+
+Portofolio.UI = {
+  init: function(){
+    
+    offset = $(".main-navbar").offset().top; //Called only once for initial offset for top
+    
+  },
+  scrolled: function(){
+      
+      
+      var curr_position=$(window).scrollTop();
+
+      if(curr_position > offset){
+        $('.main-navbar').css({'position':'fixed','top':0,height:'7%'});
+      }
+
+      else{
+        $('.main-navbar').css({'position':'relative'});
+      }
+    
+      
+  },
+  clickedScroll: function(id){
+    
+      
+      $('html, body').animate({
+          scrollTop: $("#"+id).offset().top
+      }, 2000); 
+    
+  }
   
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+
+
+
+};
+
+
+
+$(document).ready(function () {
+  Portofolio.UI.init();
+  $(window).scroll(function(){
+    Portofolio.UI.scrolled();
+  });
+  
+  $("a").click(function(event) {
+    
+    var clicked_id=event.target.id;
+    
+    clicked_id=clicked_id.replace('button','');
+    clicked_id.trim();
+    
+    Portofolio.UI.clickedScroll(clicked_id);
+  
+  });
+  
+});
+
+
+
+
