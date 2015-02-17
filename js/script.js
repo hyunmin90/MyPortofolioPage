a30cd5897a36e0db4ffd603b83c0b15d225492a0
hyunmin90 <hyunmin90@gmail.com>
2015-02-17 22:19:05 +0000

    final

--- js/script.js
+++ js/script.js
@@ -12,20 +12,40 @@ Portofolio.UI = {
       
       
       var curr_position=$(window).scrollTop();
-
+    
       if(curr_position > offset){
 	//$('.main-navbar').addClass('resize');
         //$('.main-navbar').css({'position':'fixed','top':0,width:'0%'}); Unable when I am done with MP
-        $('.main-navbar').css({'position':'fixed','top':0,width:'100%',height:'10%','padding-top':'0px','font-size':'0.6em'});
+        $('.main-navbar').css({'position':'fixed','top':0,width:'100%',height:'10%','margin-top':'0px','font-size':'0.6em'});
         //$('a').css({'padding':'0px','padding-left': '10px', 'padding-top': '20px'}); This also
       }
 
       else{
         
 	//$('.main-navbar').removeClass('resize');
-        $('.main-navbar').css({'position':'fixed','top':0,width:'100%',height:'15%','font-size':'0.9em'});
+        $('.main-navbar').css({'position':'fixed','top':0,width:'100%',height:'15%','font-size':'1.3em'});
         //$('a').css({'padding':'20px'});
       }
+    
+    if(curr_position<939){
+      $('.menu').removeClass('highlight');
+      $('#mebutton').addClass("highlight");
+      
+    }
+    else if(curr_position>939&&curr_position<1690){
+      $('.menu').removeClass('highlight');
+      $('#pastbutton').addClass("highlight");
+    }
+    else if(curr_position>1690){
+      $('.menu').removeClass('highlight');
+      $('#experiencebutton').addClass("highlight");
+    }
+    else
+      $('.menu').removeClass('highlight');
+   
+    
+      
+    
     
       
   },
@@ -70,6 +90,10 @@ $(document).ready(function () {
     var factor = $('#past').scrollLeft();
     if(factor==3612){ $('#past').animate({"scrollLeft":"0"});}
     else{$('#past').animate({"scrollLeft":"+=516"});}
+  });
+  
+  $('.experience').click(function(event){
+      $('.modalDialog').css({'display':'block'});
   });
   
 
